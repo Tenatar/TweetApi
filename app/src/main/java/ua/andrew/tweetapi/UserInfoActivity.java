093a3d729f957325eb157dc3344e2d2a9f7f7bf2
Andrew <andrewart.kaplii@gmail.com>
2020-03-21 15:20:06 +0200

    added picasso logo

--- app/src/main/java/ua/andrew/tweetapi/UserInfoActivity.java
+++ app/src/main/java/ua/andrew/tweetapi/UserInfoActivity.java
@@ -3,12 +3,18 @@ package ua.andrew.tweetapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class UserInfoActivity extends AppCompatActivity {
+    private ImageView userImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        userImageView=findViewById(R.id.user_image_view);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(userImageView);
     }
 }
